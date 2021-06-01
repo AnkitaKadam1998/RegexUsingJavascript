a067ef2e2c93a8dd2be51615349b6f7cb4c66531
komal@komal405.com <ankita80kadam@gmail.com>
2021-06-01 17:01:18 +0530

    Create regex pattern to validate pin code

--- PinCode.js
+++ PinCode.js
@@ -1,1 +1,17 @@
-class 
+function is_usZipCode(str)
+{
+regexp = /^[1-9]{1}[0-9]{2}[ ]?[0-9]{3}$/;
+
+        if (regexp.test(str))
+          {
+            return true;
+          }
+        else
+          {
+            return false;
+          }
+}
+
+console.log(is_usZipCode("400088"));
+
+ 
